7af3ea89249a6df80f756559c14f676327cb7f66
mustafa <mustafa.bhm3@gmail.com>
2022-09-07 20:28:44 -0600

    Displaying Appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Empty from "./Empty";
+import Show from "./Show";
 
 export default function Appointment(props) {
   return (
     <article className="appointment">
-      <Header />
-      <Empty />
+      <Header time={props.time} />
+      {props.interview && (
+        <Show
+          interviewer={props.interview.interviewer}
+          student={props.interview.student}
+        />
+      )}
+      {!props.interview && <Empty />}
     </article>
   );
 }
