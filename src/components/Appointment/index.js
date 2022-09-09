c9aee89325434cf25de46e7618bdf612234a43b4
mustafa <mustafa.bhm3@gmail.com>
2022-09-09 16:18:22 -0600

    implement useVisualMode to Appoointment with condi mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,18 +3,30 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Empty from "./Empty";
 import Show from "./Show";
+import Form from "./Form";
+import { useVisualMode } from "hooks/useVisualMode";
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview && (
+      {/* {props.interview && ( */}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
         <Show
-          interviewer={props.interview.interviewer}
+          // interviewer={props.interview.interviewer}
           student={props.interview.student}
+          interviewer={props.interview.interviewer.name}
         />
       )}
-      {!props.interview && <Empty />}
+      {/* {!props.interview && <Empty />} */}
+      {mode === CREATE && <Form interviewers={[]} onCancel={back} />}
     </article>
   );
 }
