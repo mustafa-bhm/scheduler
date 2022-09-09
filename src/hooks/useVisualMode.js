657053ccd1e564eb21858a966bda064690a13a63
mustafa <mustafa.bhm3@gmail.com>
2022-09-09 14:22:41 -0600

    adding useVisualMode hook & test data

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  // to transition from one mode to another
+  function transition(mode, replace = false) {
+    if (replace) {
+      return setMode(mode);
+    }
+
+    setMode(mode);
+    setHistory([...history, mode]);
+  }
+
+  // go back from one mode to the previous one
+  function back() {
+    if (history.length - 1) {
+      setHistory((prev) => {
+        const newArr = [...prev];
+
+        newArr.pop();
+
+        setMode(newArr[newArr.length - 1]);
+
+        return newArr;
+      });
+    }
+  }
+
+  return { mode, transition, back, history };
+}
