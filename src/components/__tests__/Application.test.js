df94f6a508b5f9334cc03410804fedb9bbf29375
mustafa <mustafa.bhm3@gmail.com>
2022-09-13 19:04:12 -0600

    updating axios.js with PUT req & adding tests to application.test.js

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -9,6 +9,7 @@ import {
 import { waitForElement } from "@testing-library/react";
 import { fireEvent } from "@testing-library/react";
 import { prettyDOM } from "@testing-library/react";
+import { getAllByTestId, getByPlaceholderText } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -37,6 +38,17 @@ describe("Application", () => {
   it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
     const { container } = render(<Application />);
     await waitForElement(() => getByText(container, "Archie Cohen"));
-    console.log("====", prettyDOM(container));
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" },
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(appointment, "Save"));
+    console.log(prettyDOM(appointment));
   });
 });
